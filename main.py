6c68aff5b1f085a337a8c8e72f241c09b0bff608
amazon01FireStick <95485749+amazon01FireStick@users.noreply.github.com>
2021-12-11 18:32:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -125,3 +125,48 @@ value=(value.split(" "))[0]
 char=value[0]
 print(ord(char))
 
+#calculator
+def addno(a, b):
+    s = a + b
+    print(s)
+    return s
+
+
+def subno(a, b):
+    s = a - b
+    print(s)
+    return s
+
+
+def multiply(a, b):
+    s = a * b
+    print(s)
+    return s
+
+
+def divideno(a, b):
+    s = a / b
+    print(s)
+    return s
+
+
+while True:
+    total = 0.0
+    a = float(input(""))
+    symbol = input("Enter the symbol")
+    b = float(input(""))
+    if symbol=="null":
+        break
+    if symbol == "+":
+        sum=addno(a, b)
+        total=total+sum
+    elif symbol == "-":
+        diff=subno(a, b)
+        total=total-diff
+    elif symbol == "*":
+        mul=multiply(a, b)
+        total=total*mul
+    elif symbol == "/":
+        div=divideno(a, b)
+        total=total/div
+
